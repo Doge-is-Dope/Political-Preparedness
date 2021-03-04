7505993c76d81d49d11c7b60dc0e371b222e374f
doge-is-dope <maple3312936@outlook.com>
2021-03-04 21:30:55 +0800

    chore: update lib versions

---
+++ app/src/main/java/com/example/android/politicalpreparedness/BaseApplication.kt
@@ -0,0 +1,10 @@
+package com.example.android.politicalpreparedness
+
+import android.app.Application
+
+class BaseApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
