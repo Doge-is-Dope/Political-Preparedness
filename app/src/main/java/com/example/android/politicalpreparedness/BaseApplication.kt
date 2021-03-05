4d1af13237a849d42b7780b45ea7ef9834fb38cc
clement <chunchieh.liang@coolbitx.com>
2021-03-05 10:31:18 +0800

    chore: add timber for logging

--- app/src/main/java/com/example/android/politicalpreparedness/BaseApplication.kt
+++ app/src/main/java/com/example/android/politicalpreparedness/BaseApplication.kt
@@ -1,10 +1,15 @@
 package com.example.android.politicalpreparedness
 
 import android.app.Application
+import timber.log.Timber
 
 class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
 }
